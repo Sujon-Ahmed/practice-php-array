9577937afbac3c92f340f376f588ed1c17594912
Sujon Ahmed <sujonahmed5284@gmail.com>
2021-06-22 20:04:17 +0600

    learn array_keys function

---
+++ array_key.php
@@ -0,0 +1,19 @@
+<?php
+
+    // learn array_key
+
+    // array_keys() use for see all key in a array
+    // array_key_first() use for see first key in a array
+    // array_key_last() use for see last key in a array
+    //array_key_exists('key',$array) use for search key in a array
+    //key_exists() use for search key in a array
+
+    $array = ['red','green','blue','brown'];
+    echo "<pre>";
+    print_r(array_keys($array));
+    echo "</pre>";
+
+
+
+
+?>

223d169d2d8e531449865bbbe7967db3233d901c
Sujon Ahmed <sujonahmed5284@gmail.com>
2021-06-22 20:47:16 +0600

    array_unshift

--- array_shift_unshift.php
+++ array_shift_unshift.php
@@ -2,11 +2,18 @@
 
      //array_shift() use for delete a value at first index or key
 
-     $cars = ['volvo', 'BMW', 'ferrari'];
-     array_shift($cars);
-     echo "<pre>";
-     print_r($cars);
-     echo "<pre>";
+    // $cars = ['volvo', 'BMW', 'ferrari'];
+    //  array_shift($cars);
+    //  echo "<pre>";
+    //  print_r($cars);
+    //  echo "<pre>"; 
+
+    //array_unshift() use for add  value at first index or key
+    $colors = ['green', 'red', 'yellow'];
+    array_unshift($colors,'pink','teals');
+    echo "<pre>";
+     print_r($colors);
+     echo "<pre>"; 
 
 
 ?>

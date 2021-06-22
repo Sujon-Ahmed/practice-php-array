9d355bd0eab1b529a6a00da5a8ae7efbad20b82e
Sujon Ahmed <sujonahmed5284@gmail.com>
2021-06-22 20:44:35 +0600

    array_shift

---
+++ array_shift_unshift.php
@@ -0,0 +1,12 @@
+<?php
+
+     //array_shift() use for delete a value at first index or key
+
+     $cars = ['volvo', 'BMW', 'ferrari'];
+     array_shift($cars);
+     echo "<pre>";
+     print_r($cars);
+     echo "<pre>";
+
+
+?>

ec331d3ee7572dde4b94c69611b7236cd3d4cdc7
Sujon Ahmed <sujonahmed5284@gmail.com>
2021-06-22 20:18:12 +0600

    array_merge_recursive function

--- array_merge_combine.php
+++ array_merge_combine.php
@@ -5,13 +5,26 @@ array_combine() -> index array
 -->
 <?php
     // array_merge() use for add two or many array
-    $a = ['a' => 'sujon', 'b' => 'ahmed', 'c' => 'riman'];
-    $b = ['d' => 'alamin','toukir'];
+    // $a = ['a' => 'sujon', 'b' => 'ahmed', 'c' => 'riman'];
+    // $b = ['d' => 'alamin','toukir'];
      
-    $newArray = array_merge($a,$b);
+    // $newArray = array_merge($a,$b);
 
-    echo"<pre>";
-    print_r($newArray);
-    echo"</pre>";
-    
+    // echo"<pre>";
+    // print_r($newArray);
+    // echo"</pre>";
+
+     // array_merge_recursive() use for add two or many multidimensional array
+     // if have any common index or key it's make multidimensional array
+
+    // $a = ['a'=>'sujon','b'=>'ahmed','c'=>'riman'];
+    // $b = ['b'=> ['color'=>['red','green']],'sumon'];
+    // $newArray = array_merge_recursive($a,$b);
+
+    // echo"<pre>";
+    // print_r($newArray);
+    // echo"</pre>";
+
+
+
 ?>
